527722c08b319ef2906686b1679ad99c04616814
childeYin <zhangjun516@126.com>
2018-07-07 15:16:26 +0800

    del auto create db

--- gospider.go
+++ gospider.go
@@ -108,7 +108,6 @@ func (gs *GoSpider) init() {
 	gs.mysql.User = "root"
 	gs.mysql.MaxIdleConns = 3
 	gs.mysql.MaxOpenConns = 10
-	///gs.mysql.DBName = "gospider"
 
 	gs.web = &web.Server{Port: 8080}
 }
