ea361e976e3b34737dfc0cccb009b0b38bad3b69
yongjun_lkjh <yong_ee@163.com>
2018-08-24 16:57:22 +0800

    compress js by delete static

--- web/static/build/webpack.prod.conf.js
+++ web/static/build/webpack.prod.conf.js
@@ -90,15 +90,15 @@ const webpackConfig = merge(baseWebpackConfig, {
       }
     }),
     // keep module.id stable when vender modules does not change
-    new webpack.HashedModuleIdsPlugin(),
+    new webpack.HashedModuleIdsPlugin()
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, '../static'),
+    //     to: config.build.assetsSubDirectory,
+    //     ignore: ['.*']
+    //   }
+    // ])
   ],
   optimization: {
     splitChunks: {
