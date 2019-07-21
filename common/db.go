e75388f7e2eb243b68ebcd74469a23461161c267
nange <cctonange@gmail.com>
2019-07-21 18:21:25 +0800

    common: create database if not exists

--- common/db.go
+++ common/db.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	// import the mysql driver
-	_ "github.com/go-sql-driver/mysql"
-
+	"github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -26,12 +24,24 @@ type MySQLConf struct {
 
 // NewGormDB return a new gorm db instance
 func NewGormDB(conf MySQLConf) (*gorm.DB, error) {
-	args := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		conf.User, conf.Password, conf.Host, conf.Port, conf.DBName)
-	db, err := gorm.Open("mysql", args)
+	dsn := getDSNWithDB(conf)
+	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		code, ok := GetSQLErrCode(err)
+		if !ok {
+			return nil, errors.WithStack(err)
+		}
+		if code == 1049 { // Database not exists
+			if err := createDatabase(conf); err != nil {
+				return nil, err
+			}
+		}
+		db, err = gorm.Open("mysql", dsn)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
 	}
+
 	if conf.MaxIdleConns == 0 {
 		db.DB().SetMaxIdleConns(3)
 	}
@@ -47,9 +57,8 @@ func NewGormDB(conf MySQLConf) (*gorm.DB, error) {
 
 // NewDB returns a new sql.DB instance
 func NewDB(conf MySQLConf) (*sql.DB, error) {
-	args := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		conf.User, conf.Password, conf.Host, conf.Port, conf.DBName)
-	db, err := sql.Open("mysql", args)
+	dsn := getDSNWithDB(conf)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -59,4 +68,44 @@ func NewDB(conf MySQLConf) (*sql.DB, error) {
 	db.SetMaxOpenConns(10)
 
 	return db, nil
+}
+
+func getbaseDSN(conf MySQLConf) string {
+	dsn := fmt.Sprintf("%s:%s@(%s:%d)/?charset=utf8mb4&parseTime=True&loc=Local",
+		conf.User, conf.Password, conf.Host, conf.Port)
+
+	return dsn
+}
+
+func getDSNWithDB(conf MySQLConf) string {
+	dsn := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		conf.User, conf.Password, conf.Host, conf.Port, conf.DBName)
+
+	return dsn
+}
+
+// GetSQLErrCode returns error code if err is a mysql error
+func GetSQLErrCode(err error) (int, bool) {
+	mysqlErr, ok := errors.Cause(err).(*mysql.MySQLError)
+	if !ok {
+		return -1, false
+	}
+
+	return int(mysqlErr.Number), true
+}
+
+func createDatabase(conf MySQLConf) error {
+	dsn := getbaseDSN(conf)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + conf.DBName)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
 }
